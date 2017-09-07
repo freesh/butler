1977594d909c809b85455f2e31e29d21c1c82a5e
Björn Biege <biege@sitegeist.de>
2017-09-07 10:24:02 +0200

    WIP: add additional filesystem tasks chown, chgrp, rename, symlink, hardlink, readlink, makePathRelative, mirror, isAbsolutePath, tempnam, dumpFile and appendToFile

--- src/Butler/Project/FilesystemProject.php
+++ src/Butler/Project/FilesystemProject.php
@@ -87,5 +87,242 @@ class FilesystemProject extends AbstractProject
                 'recursive' => true // (optional) bool Whether change the mod recursively or not (default: false)
             ],
         ]);
+
+        $this->addTask([
+            'key' => 'chown',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'chown',
+            'options' => [
+                'files' => 'lala', // string|array|\Traversable A filename, an array of files, or a \Traversable instance to change owner
+                'user' => 'freesh', // string The new owner user name
+                'recursive' => true // (optional) bool Whether change the owner recursively or not (default: false)
+            ],
+        ]);
+
+        /**
+         * Change the group of an array of files or directories.
+         *
+         * @param string|array|\Traversable $files     A filename, an array of files, or a \Traversable instance to change group
+         * @param string                    $group     The group name
+         * @param bool                      $recursive Whether change the group recursively or not
+         *
+         * @throws IOException When the change fail
+         */
+        $this->addTask([
+            'key' => 'chgrp',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'chgrp',
+            'options' => [
+                'files' => 'lala', // string|array|\Traversable A filename, an array of files, or a \Traversable instance to change group
+                'group' => 'staff', // string The group name
+                'recursive' => true // (optional) bool Whether change the group recursively or not (default: false)
+            ],
+        ]);
+
+        /**
+         * Renames a file or a directory.
+         *
+         * @param string $origin    The origin filename or directory
+         * @param string $target    The new filename or directory
+         * @param bool   $overwrite Whether to overwrite the target if it already exists
+         *
+         * @throws IOException When target file or directory already exists
+         * @throws IOException When origin cannot be renamed
+         */
+        $this->addTask([
+            'key' => 'rename',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'rename',
+            'options' => [
+                'origin' => 'lala', // string The origin filename or directory
+                'target' => 'staff', // string The new filename or directory
+                'overwrite' => true // (optional) bool Whether to overwrite the target if it already exists (default: false)
+            ],
+        ]);
+
+        /**
+         * Creates a symbolic link or copy a directory.
+         *
+         * @param string $originDir     The origin directory path
+         * @param string $targetDir     The symbolic link name
+         * @param bool   $copyOnWindows Whether to copy files if on Windows
+         *
+         * @throws IOException When symlink fails
+         */
+        $this->addTask([
+            'key' => 'symlink',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'symlink',
+            'options' => [
+                'originDir' => 'staff', // string The origin directory path
+                'targetDir' => 'symlink', // string The symbolic link name
+                'copyOnWindows' => true // (optional) bool Whether to copy files if on Windows (default: false)
+            ],
+        ]);
+
+        /**
+         * Creates a hard link, or several hard links to a file.
+         *
+         * @param string          $originFile  The original file
+         * @param string|string[] $targetFiles The target file(s)
+         *
+         * @throws FileNotFoundException When original file is missing or not a file
+         * @throws IOException           When link fails, including if link already exists
+         */
+        $this->addTask([
+            'key' => 'hardlink',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'hardlink',
+            'options' => [
+                'originFile' => 'test.txt', // string The original file
+                'targetFiles' => ['hardlink','hardlink2','hardlink3'], // string|array The target file(s)
+            ],
+        ]);
+
+        /**
+         * Resolves links in paths.
+         *
+         * With $canonicalize = false (default)
+         *      - if $path does not exist or is not a link, returns null
+         *      - if $path is a link, returns the next direct target of the link without considering the existence of the target
+         *
+         * With $canonicalize = true
+         *      - if $path does not exist, returns null
+         *      - if $path exists, returns its absolute fully resolved final version
+         *
+         * @param string $path         A filesystem path
+         * @param bool   $canonicalize Whether or not to return a canonicalized path
+         *
+         * @return string|null
+         */
+        $this->addTask([
+            'key' => 'readlink',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'readlink',
+            'options' => [
+                'path' => 'symlink', // string The original file
+                'canonicalize' => true, // (optional) bool Whether or not to return a canonicalized path (default: false)
+            ],
+        ]);
+
+        /**
+         * Given an existing path, convert it to a path relative to a given starting path.
+         *
+         * @param string $endPath   Absolute path of target
+         * @param string $startPath Absolute path where traversal begins
+         *
+         * @return string Path of target relative to starting path
+         */
+        $this->addTask([
+            'key' => 'makePathRelative',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'makePathRelative',
+            'options' => [
+                'endPath' => '/Users/freesh/PhpstormProjects/butler/', // string Absolute path of target
+                'startPath' => '/Users/freesh/PhpstormProjects/butler/testfolder', // string Absolute path where traversal begins
+            ],
+        ]);
+
+        /**
+         * Mirrors a directory to another.
+         *
+         * @param string       $originDir The origin directory
+         * @param string       $targetDir The target directory
+         * @param \Traversable $iterator  A Traversable instance
+         * @param array        $options   An array of boolean options
+         *                                Valid options are:
+         *                                - $options['override'] Whether to override an existing file on copy or not (see copy())
+         *                                - $options['copy_on_windows'] Whether to copy files instead of links on Windows (see symlink())
+         *                                - $options['delete'] Whether to delete files that are not in the source directory (defaults to false)
+         *
+         * @throws IOException When file type is unknown
+         */
+        $this->addTask([
+            'key' => 'mirror',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'mirror',
+            'options' => [
+                'originDir' => 'staff', // string The origin directory
+                'targetDir' => 'lulu', // string The target directory
+                'iterator' => null, // (optional) \Traversable A Traversable instance (default: null)
+                'options' => [ // (optional) array An array of boolean options (default: array())
+                    'override' => false, // (optional) bool Whether to override an existing file on copy or not (see copy()) (default: false)
+                    'copy_on_windows' => true, // (optional) bool Whether to copy files instead of links on Windows (see symlink()) (default: false)
+                    'delete' => true // (optional) bool Whether to delete files that are not in the source directory (defaults to false)
+                ]
+            ],
+        ]);
+
+        /**
+         * Returns whether the file path is an absolute path.
+         *
+         * @param string $file A file path
+         *
+         * @return bool
+         */
+        $this->addTask([
+            'key' => 'isAbsolutePath',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'isAbsolutePath',
+            'options' => [
+                'file' => 'copy.txt', // string A file path
+            ],
+        ]);
+
+        /**
+         * ToDo: should someone use this? remove this task?
+         * Creates a temporary file with support for custom stream wrappers.
+         *
+         * @param string $dir    The directory where the temporary filename will be created
+         * @param string $prefix The prefix of the generated temporary filename
+         *                       Note: Windows uses only the first three characters of prefix
+         *
+         * @return string The new temporary filename (with path), or throw an exception on failure
+         */
+        $this->addTask([
+            'key' => 'tempnam',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'tempnam',
+            'options' => [
+                'dir' => 'temp', // string The directory where the temporary filename will be created
+                'prefix' => 'tmp_' // string The prefix of the generated temporary filename (Note: Windows uses only the first three characters of prefix)
+            ],
+        ]);
+
+        /**
+         * Atomically dumps content into a file.
+         *
+         * @param string $filename The file to be written to
+         * @param string $content  The data to write into the file
+         *
+         * @throws IOException If the file cannot be written to
+         */
+        $this->addTask([
+            'key' => 'dumpFile',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'dumpFile',
+            'options' => [
+                'filename' => 'test.txt', // string The file to be written to
+                'content' => 'dumped content... :)' // string The data to write into the file
+            ],
+        ]);
+
+        /**
+         * Appends content to an existing file.
+         *
+         * @param string $filename The file to which to append content
+         * @param string $content  The content to append
+         *
+         * @throws IOException If the file is not writable
+         */
+        $this->addTask([
+            'key' => 'appendToFile',
+            'class' => '\\Butler\\Task\\FilesystemTask',
+            'task' => 'appendToFile',
+            'options' => [
+                'filename' => 'test.txt', // string The file to which to append content
+                'content' => 'appended content... :-)' // string The content to append
+            ],
+        ]);
     }
 }
--- src/Butler/Task/FilesystemTask.php
+++ src/Butler/Task/FilesystemTask.php
@@ -32,7 +32,7 @@ class FilesystemTask extends AbstractTask
         $this->fs->copy(
             $config['options']['originFile'],
             $config['options']['targetFile'],
-            (!isset($config['options']['overwriteNewerFiles']) ? true : false)
+            (isset($config['options']['overwriteNewerFiles']) ? $config['options']['overwriteNewerFiles'] : false)
         );
     }
 
@@ -42,7 +42,7 @@ class FilesystemTask extends AbstractTask
     public function mkdir(array $config) {
         $this->fs->mkdir(
             $config['options']['dirs'],
-            (!isset($config['options']['mode']) ? $config['options']['mode'] : 0777)
+            (isset($config['options']['mode']) ? $config['options']['mode'] : 0777)
         );
     }
 
@@ -60,8 +60,8 @@ class FilesystemTask extends AbstractTask
     public function touch(array $config) {
         $this->fs->touch(
             $config['options']['files'],
-            (!isset($config['options']['time']) ? $config['options']['time'] : null),
-            (!isset($config['options']['atime']) ? $config['options']['atime'] : null)
+            (isset($config['options']['time']) ? $config['options']['time'] : null),
+            (isset($config['options']['atime']) ? $config['options']['atime'] : null)
         );
     }
 
@@ -77,12 +77,170 @@ class FilesystemTask extends AbstractTask
     /**
      * @param array $config
      */
-    public function chmod(array $config) {
+    public function chmod(array $config){
         $this->fs->chmod(
             $config['options']['files'],
             $config['options']['mode'],
-            (!isset($config['options']['umask']) ? $config['options']['umask'] : 0000),
-            (!isset($config['options']['recursive']) ? $config['options']['recursive'] : false)
+            (isset($config['options']['umask']) ? $config['options']['umask'] : 0000),
+            (isset($config['options']['recursive']) ? $config['options']['recursive'] : false)
+        );
+    }
+
+
+    /**
+     * @param array $config
+     */
+    public function chown(array $config) {
+        $this->fs->chown(
+            $config['options']['files'],
+            $config['options']['user'],
+            (isset($config['options']['recursive']) ? $config['options']['recursive'] : false)
+        );
+    }
+
+    /**
+     * @param array $config
+     */
+    public function chgrp(array $config) {
+        $this->fs->chgrp(
+            $config['options']['files'],
+            $config['options']['group'],
+            (isset($config['options']['recursive']) ? $config['options']['recursive'] : false)
+        );
+    }
+
+    /**
+     * @param array $config
+     */
+    public function rename(array $config) {
+        $this->fs->rename(
+            $config['options']['origin'],
+            $config['options']['target'],
+            (isset($config['options']['overwrite']) ? $config['options']['overwrite'] : false)
+        );
+    }
+
+    /**
+     * @param array $config
+     */
+    public function symlink(array $config) {
+        $this->fs->symlink(
+            $config['options']['originDir'],
+            $config['options']['targetDir'],
+            (isset($config['options']['copyOnWindows']) ? $config['options']['copyOnWindows'] : false)
+        );
+    }
+
+    /**
+     * @param array $config
+     */
+    public function hardlink(array $config) {
+        $this->fs->hardlink(
+            $config['options']['originFile'],
+            $config['options']['targetFiles']
+        );
+    }
+
+    /**
+     * ToDo: how to use returned value?
+     * Resolves links in paths.
+     *
+     * @param array   $config
+     *
+     * @return string|null
+     */
+    public function readlink(array $config) {
+        return $this->fs->readlink(
+            $config['options']['path'],
+            (isset($config['options']['canonicalize']) ? $config['options']['canonicalize'] : false)
+        );
+    }
+
+    /**
+     * ToDo: how to use returned value?
+     * Given an existing path, convert it to a path relative to a given starting path.
+     *
+     * @param array $config
+     *
+     * @return string Path of target relative to starting path
+     */
+    public function makePathRelative(array $config) {
+        return $this->fs->makePathRelative(
+            $config['options']['endPath'],
+            $config['options']['startPath']
+        );
+    }
+
+    /**
+     * Mirrors a directory to another.
+     *
+     * @param array $config
+     *
+     * @throws IOException When file type is unknown
+     */
+    public function mirror(array $config) {
+        $this->fs->mirror(
+            $config['options']['originDir'],
+            $config['options']['targetDir'],
+            (isset($config['options']['iterator']) ? $config['options']['iterator'] : null),
+            (isset($config['options']['options']) ? $config['options']['options'] : array())
+        );
+    }
+
+    /**
+     * ToDo: how to use returned value?
+     * Returns whether the file path is an absolute path.
+     *
+     * @param array $config
+     *
+     * @return bool
+     */
+    public function isAbsolutePath(array $config) {
+        return $this->fs->isAbsolutePath(
+            $config['options']['file']
+        );
+    }
+
+    /**
+     * ToDo: how to use returned value?
+     * Creates a temporary file with support for custom stream wrappers.
+     *
+     * @param array $config
+     *
+     * @return string The new temporary filename (with path), or throw an exception on failure
+     */
+    public function tempnam(array $config) {
+        return $this->fs->tempnam(
+            $config['options']['dir'],
+            $config['options']['prefix']
+        );
+    }
+
+    /**
+     * Atomically dumps content into a file.
+     *
+     * @param array $config
+     *
+     * @throws IOException If the file cannot be written to
+     */
+    public function dumpFile(array $config) {
+        $this->fs->dumpFile(
+            $config['options']['filename'],
+            $config['options']['content']
+        );
+    }
+
+    /**
+     * Appends content to an existing file.
+
+     * @param array $config
+     *
+     * @throws IOException If the file is not writable
+     */
+    public function appendToFile(array $config) {
+        $this->fs->appendToFile(
+            $config['options']['filename'],
+            $config['options']['content']
         );
     }
 }
