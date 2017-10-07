0f34be0f1618c481a44735ce61e93e7c43c0fd37
freesh <freeshsolutions@googlemail.com>
2017-10-07 02:33:56 +0200

    Task: Neos Project: Create docker-compose.yaml

--- src/Butler/Project/NeosBaseProject.php
+++ src/Butler/Project/NeosBaseProject.php
@@ -102,6 +102,58 @@ class NeosBaseProject extends AbstractProject
             ]
         ]);
 
+
+        $this->addTask([
+            'key' => 'Create docker-compose.yml',
+            'class' => '\\Butler\\Task\\NeosTask',
+            'task' => 'settings',
+            'options' => [
+                'filename' => 'docker-compose.yml', // string path to Settings.yaml; Existing file will be merged.
+                'settings' => [ // array with setting structure
+                    'version' => '2',
+                    'services' => [
+                        'db' => [
+                            'image' => 'mysql:5.7',
+                            'volumes' => [
+                                "./Data/Docker/Database:/var/lib/mysql"
+                            ],
+                            'restart' => 'always',
+                            'ports' => [
+                                '8086:3306'
+                            ],
+                            'command' => [
+                                '--character-set-server=utf8',
+                                '--collation-server=utf8_unicode_ci'
+                            ],
+                            'environment' => [
+                                'MYSQL_ROOT_PASSWORD' => 'toor',
+                                'MYSQL_DATABASE' => 'application',
+                                'MYSQL_USER' => 'toor',
+                                'MYSQL_PASSWORD' => 'toor',
+                            ],
+                        ],
+                        'phpmyadmin' => [
+                            'image' => 'phpmyadmin/phpmyadmin',
+                            'environment' => [
+                                'PMA_HOST=db',
+                                'PMA_PORT=3306',
+                                'PMA_USER=toor',
+                                'PMA_PASSWORD=toor'
+                            ],
+                            'restart' => 'always',
+                            'ports' => [
+                                '8082:80'
+                            ],
+                            'volumes' => [
+                                '/sessions'
+                            ]
+                        ],
+                    ]
+                ]
+            ]
+        ]);
+
+
 /*
         // init docker
         $this->addTask([
