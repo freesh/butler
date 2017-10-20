71aef33ba0254ba1cf6999f5f164a84bd37ddca8
Björn Biege <biege@sitegeist.de>
2017-10-20 19:04:38 +0200

    Task: add neos-task: createUser

--- src/Butler/Task/NeosTask.php
+++ src/Butler/Task/NeosTask.php
@@ -77,4 +77,22 @@ class NeosTask extends AbstractTask
         $this->execute('export FLOW_CONTEXT='. $context .' && ./flow doctrine:migrate');
 
     }
+
+
+    /**
+     * @param array $config
+     */
+    public function createUser(array $config) {
+
+        $context = 'Development';
+
+        // set context to Production
+        if ( isset($config['options']['context'])) {
+            $context = $config['options']['context'];
+        }
+
+        // execute command
+        $this->execute('export FLOW_CONTEXT='. $context .' && ./flow user:create '. $config['options']['user'] .' '. $config['options']['password'] .' '. $config['options']['username'] . (!isset($config['options']['roles']) ? '' : ' --roles ' . implode(' ', $config['options']['roles'])) );
+
+    }
 }
